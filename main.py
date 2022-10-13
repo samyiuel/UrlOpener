0acb873d407a79d10e5966ec8330191fb0406647
Samuel Xavier <66645510+samyiuel@users.noreply.github.com>
2022-10-13 18:11:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 # Import lib
-import webbrowser
 
 # TODO: Create function to url opener
-webbrowser.open(url)
+import webbrowser
+webbrowser.open('https://www.google.com/')
+webbrowser.open('https://www.spotify.com/')
+webbrowser.open('https://www.youtube.com/')
 
 # TODO: Implement foreach to open multiples urls
 
