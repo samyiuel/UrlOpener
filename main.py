5718056f07b58e6436ef08539bdaf848e5342933
Samuel Xavier <66645510+samyiuel@users.noreply.github.com>
2022-10-14 14:32:28 -0300

    Fix script

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-# Import lib
-
-# TODO: Create function to url opener
 import webbrowser
-webbrowser.open('https://www.google.com/')
-webbrowser.open('https://www.spotify.com/')
-webbrowser.open('https://www.youtube.com/')
 
-# TODO: Implement foreach to open multiples urls
+urls = ["https://github.com/", "https://www.google.com/", "https://gmail.com/"]
 
-# TODO: Call the function
+for url in urls:
+    webbrowser.open(url)
